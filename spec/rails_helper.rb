f23a396183ea752a39edeecfcf3ef2ddfc3c284d
a_bahaa <ahmedbahaa_11@hotmail.com>
2018-02-22 11:02:57 +0200

    Add controllers todo & item with testCase

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -18,6 +19,11 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.before(:all) do
+    FactoryBot.reload
+  end
+
+  config.include RequestSpecHelper
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
